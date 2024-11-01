d661f006e6aae9e80fe8eec08d7f0ee8a642c24a
Sylvie Graff <segraff@cps.edu>
2024-11-01 10:11:55 -0500

    finished a4.py

--- a4.py
+++ a4.py
@@ -4,12 +4,45 @@
 
 class TTTBoard:
     """A tic tac toe board
+   
 
     Attributes:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+    def __init__(self):
+        self.board = ['*'] * 9
 
+    def __str__(self):
+        return f"{self.board[0]} {self.board[1]} {self.board[2]}\n" \
+               f"{self.board[3]} {self.board[4]} {self.board[5]}\n" \
+               f"{self.board[6]} {self.board[7]} {self.board[8]}"
+
+    def make_move(self, player, pos):
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+        self.board[pos] = player
+        return True
+
+    def has_won(self, player):
+        winning_combinations = [
+            [0, 1, 2],  # top 
+            [3, 4, 5],  # middle
+            [6, 7, 8],  # bottom
+            [0, 3, 6],  # left
+            [1, 4, 7],  # middle col
+            [2, 5, 8],  # right 
+            [0, 4, 8],  # diagonal
+            [2, 4, 6],  # anti-diagonal
+        ]
+        
+        return any(all(self.board[pos] == player for pos in combo) for combo in winning_combinations)
+
+    def game_over(self):
+        return self.has_won('X') or self.has_won('O') or '*' not in self.board
+
+    def clear(self):
+        self.board = ['*'] * 9
     pass
 
 
@@ -88,5 +121,4 @@ if __name__ == "__main__":
 
     print("All tests passed!")
 
-    # uncomment to play!
-    # play_tic_tac_toe()
+play_tic_tac_toe()
--- notes.py
+++ notes.py
@@ -2,3 +2,41 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    species = "canis familiaris"
+
+    def __init__(self, n = "no name", a=0):
+        self.name = n
+        self.age = a
+        self.fetch_count = 0
+
+    def __str__(self):
+        s = f"{self.name} is {self.age} years old"
+        return s 
+    
+    def play_fetch(self, num_fetch):
+        self.fetch_count += num_fetch
+    
+    def fetch_reset(self):
+        self.fetch_count = 0 
+
+logan = Dog("Logan", 8)
+becker = Dog("Becker", 4)
+kepa = Dog("Kepa", 4)
+
+
+print(logan.fetch_count)
+#print(becker)
+#print(kepa)
+
+logan.play_fetch(5)
+
+print(logan.fetch_count)
+
+logan.play_fetch(3)
+
+print(logan.fetch_count)
+
+logan.fetch_reset()
+
+print(logan.fetch_count)
